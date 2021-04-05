b12eb47594d2ac0364ece8be0770fc4c07541298
Thibaut Vercueil <thibaut.vercueil@pm.me>
2021-04-06 00:05:59 +0200

    Add first FEN Reading implementation

--- src/board/mod.rs
+++ src/board/mod.rs
@@ -44,6 +44,52 @@ impl Board {
         return board;
     }
 
+    pub fn from_fen(fen_string: &String) -> Self {
+        let splits: Vec<&str> = fen_string.split(' ').collect();
+        //TODO: Test len of splits
+
+        let ranks: Vec<&str> = splits[0].split('/').collect();
+        //TODO: Test len of ranks
+
+        let mut init_pieces = [None; 64];
+
+        for (rank, rank_str) in ranks.iter().enumerate() {
+            let rank = 7 - rank;
+            let mut file: usize = 0;
+            for c in rank_str.chars() {
+                print!("{}", c);
+                let piece = match c {
+                    'p' => make_piece(Color::Black, Kind::Pawn),
+                    'n' => make_piece(Color::Black, Kind::Knight),
+                    'b' => make_piece(Color::Black, Kind::Bishop),
+                    'r' => make_piece(Color::Black, Kind::Rook),
+                    'k' => make_piece(Color::Black, Kind::King),
+                    'q' => make_piece(Color::Black, Kind::Queen),
+                    'P' => make_piece(Color::White, Kind::Pawn),
+                    'N' => make_piece(Color::White, Kind::Knight),
+                    'B' => make_piece(Color::White, Kind::Bishop),
+                    'R' => make_piece(Color::White, Kind::Rook),
+                    'K' => make_piece(Color::White, Kind::King),
+                    'Q' => make_piece(Color::White, Kind::Queen),
+                    x => {
+                        let num_blanks = x.to_digit(10).unwrap();
+                        file += (num_blanks - 1) as usize;
+                        None
+                    }
+                };
+                init_pieces[rank * 8 + file] = piece;
+                print!(" [{}] ", rank * 8 + file);
+                file += 1;
+            }
+            println!();
+        }
+
+        Board {
+            pieces: init_pieces,
+            side_to_play: Color::White,
+        }
+    }
+
     pub fn generate_moves(&self) -> Vec<Move> {
         let mut moves: Vec<Move> = Vec::new();
 
@@ -67,18 +113,46 @@ impl Board {
 
                             // Let's see what is just if front of us
                             let target_square = (square + direction_offset) as usize;
-                            if self.pieces[target_square].is_none() {
-                                moves.push(Move::new(square, target_square as i32));
-                            }
 
-                            // Handle 2 square move on start rank
-                            if rank == start_rank {
-                                let target_square = (square + 2 * direction_offset) as usize;
-                                if self.pieces[target_square].is_none() {
-                                    moves.push(Move::new(square, target_square as i32));
+                            // If there is no piece blocking the move
+                            if self.pieces[target_square].is_none() {
+                                let target_square = target_square as i32;
+                                // If we're on the last rank
+                                if rank == promotion_rank {
+                                    moves.push(Move::new_with_promotion(
+                                        square,
+                                        target_square,
+                                        Some(Kind::Queen),
+                                    ));
+                                    moves.push(Move::new_with_promotion(
+                                        square,
+                                        target_square,
+                                        Some(Kind::Rook),
+                                    ));
+                                    moves.push(Move::new_with_promotion(
+                                        square,
+                                        target_square,
+                                        Some(Kind::Bishop),
+                                    ));
+                                    moves.push(Move::new_with_promotion(
+                                        square,
+                                        target_square,
+                                        Some(Kind::Knight),
+                                    ));
+                                } else {
+                                    // Normal case
+                                    moves.push(Move::new(square, target_square));
+                                }
+
+                                // Handle 2 square move on start rank
+                                if rank == start_rank {
+                                    let target_square = (square + 2 * direction_offset) as usize;
+                                    if self.pieces[target_square].is_none() {
+                                        moves.push(Move::new(square, target_square as i32));
+                                    }
                                 }
                             }
-                        }
+                        } // End of pawn move
                         _ => {}
                     }
                 }
--- src/main.rs
+++ src/main.rs
@@ -2,7 +2,12 @@ mod board;
 use board::*;
 
 fn main() {
-    let board: Board = Board::new();
+    // let board: Board = Board::new();
+    let board: Board = Board::from_fen(&String::from(
+        // "rnbqkbnr/pppppppp/8/8/8/8/PPPPPPPP/RNBQKBNR w KQkq - 0 1",
+        // "8/8/8/8/3Q4/8/8/8 w - - 0 1",
+        "8/8/2rq4/8/3Q4/2b5/4PPP1/8 w - - 0 1",
+    ));
 
     let moves = board.generate_moves();
 
